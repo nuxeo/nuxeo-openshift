f1d13f2e13fd6c5d64bb135c13116fa089e43111
Estelle Giuly <egiuly@nuxeo.com>
2018-11-28 15:53:35 +0100

    NXCT-104: Display http response + Move logic to ConnectClient.deployPackageToConnect

--- src/org/nuxeo/openshift/library/ConnectClient.groovy
+++ src/org/nuxeo/openshift/library/ConnectClient.groovy
@@ -12,11 +12,38 @@ import org.apache.http.impl.client.HttpClientBuilder
 
 import static org.apache.http.auth.AuthScope.ANY
 
-def getClientIdByStudioProject(connectUsername, connectPassword, studioProject) {
+// For now, this method only works with the Connect Password and does not with the Connect Token
+def deployPackageToConnect(connectUsername, connectPassword, studioProject, packageFilePath, connectUrl='https://connect.nuxeo.com') {
+  def clientId = getClientIdByStudioProject(connectUsername, connectPassword, studioProject)
+  if (clientId == null) {
+    error("No clientId found with user '" + connectUsername + "' for Studio project '" + studioProject + "'.").
+    return
+  }
+
+  def uploadUrl = connectUrl + "/nuxeo/site/marketplace/upload?batch=true\\&project=${studioProject}\\&owner=${clientId}\\&client=${clientId}"
+  def format = "CONNECT_HTTP_STATUS: %{http_code}"
+  def packageFileArg = "package=@${packageFilePath}"
+  def connectResponseFile = "connect_response.txt"
+
+  sh "curl -w '${format}' -u ${connectUsername}:${connectPassword} -i -n -F ${packageFileArg} ${uploadUrl} > ${connectResponseFile}"
+  handleResponse(connectResponseFile)
+}
+
+protected getClientIdByStudioProject(connectUsername, connectPassword, studioProject) {
   def applicableClients = queryConnectApplicableClients(connectUsername, connectPassword)
   def clientId = findClientIdByStudioProject(applicableClients, studioProject)
 
   return clientId
+}
+
+protected handleResponse(connectResponseFile) {
+  def httpStatusFile = "http_status.txt"
+  sh "cat ${connectResponseFile} | grep CONNECT_HTTP_STATUS |  awk '{print \$2}' > ${httpStatusFile}"
+  def httpStatus = readFile(httpStatusFile).trim()
+  if (httpStatus != "200") {
+    def connectResponse = readFile(connectResponseFile).trim()
+    error(connectResponse)
+  }
 }
 
 protected queryConnectApplicableClients(connectUsername, connectPassword, connectUrl='https://connect.nuxeo.com') {
