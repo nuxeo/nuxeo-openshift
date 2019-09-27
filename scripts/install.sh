336bfe15c62a87d4f6d6f2df60c981608f58c2f3
Damien Metzler <dmetzler@nuxeo.com>
2019-09-27 14:11:06 -0700

    NXCT-240: allow to install specific version of HF

--- scripts/install.sh
+++ scripts/install.sh
@@ -38,8 +38,11 @@ if [ -f /opt/nuxeo/connect/connect.properties ]; then
     echo "---> Configuring connect credentials"
     /docker-entrypoint.sh nuxeoctl register $NUXEO_CONNECT_USERNAME $NUXEO_STUDIO_PROJECT dev openshift $NUXEO_CONNECT_PASSWORD
 
-    echo "---> Installing hotfixes"
-    /docker-entrypoint.sh nuxeoctl mp-hotfix
+
+    if [ ! -n "$NUXEO_HOTFIX_VERSION" -a "false" != "$INSTALL_HOTFIX" ]; then
+      echo "---> Installing hotfixes"
+      /docker-entrypoint.sh nuxeoctl mp-hotfix
+    fi
   fi
 
   if [ -n "$NUXEO_STUDIO_PROJECT_VERSION" ]; then
@@ -64,6 +67,10 @@ fi
 
 if [ -n "$NUXEO_PACKAGES" ]; then
   ADDITIONAL_NUXEO_PACKAGES=$NUXEO_PACKAGES
+fi
+
+if [ -n "$NUXEO_HOTFIX_VERSION" -a "false" != "$INSTALL_HOTFIX" ]; then
+  ADDITIONAL_NUXEO_PACKAGES="$ADDITIONAL_NUXEO_PACKAGES nuxeo-$NUXEO_HOTFIX_VERSION"
 fi
 
 if [ "$FORCE_STUDIO_PACKAGE_INSTALL" == "True" ]; then
